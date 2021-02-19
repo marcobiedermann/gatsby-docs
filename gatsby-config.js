36a7336c8e1d13084d19c864743154e6ddd194bb
Marco Biedermann <marco.biedermann@gmx.de>
2021-02-19 16:51:53 +0100

    feat: gatsby-remark-autolink-headers
    Add gatsby-remark-autolink-headers to link headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,13 @@ module.exports = {
     },
     {
       resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-autolink-headers",
+          },
+        ],
+      },
     },
     {
       resolve: "gatsby-transformer-sharp",
